dd2bea2638ebceccff45a6e7081bfd933f882577
TonyXie <txie145@gmail.com>
2013-04-06 18:02:08 -0400

    added click listeners on tumblr

--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
 
+  // clicking like button
   $('a.post_control.like_button').click(function(e){
     alert("clicked like button");
   });
 
+  // clicking reblog button
   $('a.post_control.reblog_button').click(function(e){
-    alert("clicked like button");
+    alert("clicked reblog button");
+  });
+
+  // clicking image thumbnail
+  $('img.image_thumbnail').click(function(e){
+    alert("clicked image thumbnail");
+  });
+
+  // create post
+  $('#create_post').click(function(e){
+    alert("clicked create post");
   });
 
 });
